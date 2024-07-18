e1715fdf27e898a808da4d52b8feb6482fad437e
Cristhian Custodio <cryisthian_06@hotmail.com>
2024-07-17 21:07:04 -0500

    con rutas

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 from flask import Flask, Blueprint, redirect, render_template, url_for, request
-# from Controller.Cine import cine_blue
-# from Controller.Pelicula import pelicula_blue
-# from Controller.API import api_blue
+from Controller.Cine import cine_blue
+from Controller.Pelicula import pelicula_blue
+from Controller.API import api_blue
 
 
 app = Flask(__name__)
-# app.register_blueprint(cine_blue)
-# app.register_blueprint(pelicula_blue)
-# app.register_blueprint(api_blue)
+app.register_blueprint(cine_blue)
+app.register_blueprint(pelicula_blue)
+app.register_blueprint(api_blue)
 
 @app.route("/")
 def index():
